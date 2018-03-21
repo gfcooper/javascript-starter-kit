78793a11abfde448723ed5cd3e1e68242a90c5b1
glen ford <glen.cooper@me.com>
2018-03-21 15:07:57 +0000

    testing added

---
+++ src/index.test.js
@@ -0,0 +1,34 @@
+import {expect} from "chai"
+
+describe("Our first test", ()=>{
+    it("Should pass", ()=>{
+        expect(true).to.equal(true)
+    })
+})
+
+describe("Our first test", ()=>{
+    it("Should pass", ()=>{
+        expect(true).to.equal(true)
+    })
+})
+
+
+describe("Our first test", ()=>{
+    it("Should pass", ()=>{
+        expect(true).to.equal(false)
+    })
+})
+
+
+describe("Our first test", ()=>{
+    it("Should pass", ()=>{
+        expect(true).to.equal(true)
+    })
+})
+
+
+describe("Our first test", ()=>{
+    it("Should pass", ()=>{
+        expect(true).to.equal(true)
+    })
+})
